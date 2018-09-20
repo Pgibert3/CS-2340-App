feaf17e5b4e182436a5f9a8bc5d57f1fa411fa41
paulgibert98 <paulgibert98@gmail.com>
2018-09-20 16:44:26 -0400

    Added README

--- App.js
+++ App.js
@@ -6,44 +6,39 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {render} from 'react-dom';
+import {provider} from 'react-redux';
+import {createStore} from 'redux';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+const initialState = {
+    items: []
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const reducer = function(state=initialState, action) {
+    switch(action.type) {
+        case 'ADD_ITEM':
+            return (...state, items.concat([action.item]));
+
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+export default App extends React.Component<Props> {
+    render() {
+
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        items : state.items
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+
+}
