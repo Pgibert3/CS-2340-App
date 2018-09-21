77064fb52595adda65dd96b538ccb9820e5acb6b
paulgibert98 <paulgibert98@gmail.com>
2018-09-21 15:02:11 -0400

    Retry commit: setup Pages layout

---
+++ components/Button.js
@@ -0,0 +1,16 @@
+//Add imports here
+
+/**
+ * Custom Donatrix Button component
+ *
+ * @param title Text that displays in the button
+ * @param onPressed Callback function to be run when
+ * Button in pressed
+ *
+ * @return react-native Button component
+ */
+export default class Button extends Component<{}> {
+    render() {
+        //TODO: Return button
+    }
+}
