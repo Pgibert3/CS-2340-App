e0b131f1d62726ba8ea53b80be0ddcf422954145
paulgibert98 <paulgibert98@gmail.com>
2018-09-21 14:56:05 -0400

    Added basic pages layout

---
+++ pages/WelcomePage/WelcomePage.js
@@ -0,0 +1,22 @@
+//Add imports here
+
+
+/**
+ * WelcomePage is displayed as the apps home screen
+ * with options to login or register
+ *
+ * @return Text component for title and
+ * two Button components for login/register
+ */
+export default class WelcomePage extends Component<{}> {
+    render() {
+        //TODO: return Text for title and two Buttons for
+        //Register and login
+    }
+
+    onLoginPressed(id) {
+        //TODO: update page state of App to LoginPage
+    }
+
+    //TODO: Subscribe this component to the App store
+}
