32ddb566062901462dd7e519a393a75fd2fe6a33
paulgibert98 <paulgibert98@gmail.com>
2018-09-27 16:14:52 -0400

    Experimenting with Animation class

--- src/main/js/components/Button.js
+++ src/main/js/components/Button.js
@@ -1,27 +1,72 @@
-import React, {Component} from 'react';
-import {Button as NativeButton} from 'react-native';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  Animated,
+  Easing,
+  TouchableWithoutFeedback
+} from 'react-native';
 
-/**
- * Basic Button Component
- * @param title Text to be displayued in button
- * @param onPress Callback function for when button is pressed
- *
- * @return Returns a react-native Button with styling
- */
 export default class Button extends Component {
+    constructor(props) {
+      super(props);
+      this.scaleValue = new Animated.Value(0);
+
+      this.buttonScale = this.scaleValue.interpolate({
+          inputRange: [0, 0.5, 1],
+          outputRange: [0, 1.1, 1]
+      });
+
+      this.styles = StyleSheet.create({
+          button: {
+              alignItems: 'center',
+              padding: 40,
+              borderWidth: 1,
+              borderColor: '#FF00FF',
+              margin: 20
+          },
+          title: {
+              color: '#FF0000'
+          }
+      });
+    }
+
     render() {
-        return(
-            <NativeButton
-                title={this.props.title}
-                onPress={this.props.onPress}
-            />
+        const scale = this.scaleValue.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, 2]
+        });
+        return (
+            <TouchableWithoutFeedback onPress={this.onPress} >
+                <Animated.View style={[
+                    this.styles.button,
+                    {
+                        transform: [
+                            {scale: this.buttonScale}
+                        ]
+                    }
+                    ]}
+                >
+                <Text style={this.styles.title}>{this.props.title}</Text>
+                </Animated.View>
+            </TouchableWithoutFeedback>
         );
     }
-}
 
-//Type checking
-Button.propTypes = {
-    title: PropTypes.string.isRequired,
-    onPress: PropTypes.func,
+    scale() {
+        this.scaleValue.setValue(0);
+        Animation.timing(
+            this.scaleValue,
+            {
+                toValue: 1.5,
+                duration: 300,
+                easing: Easing.linear
+            }
+        ).start();
+    }
+
+    onPress() {
+        this.scale();
+        this.props.onPress();
+    }
 }
