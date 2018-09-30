85bcaf63117c081fd8b8164b4d30835122d96656
Pgibert3 <pgibert3@gatech.edu>
2018-09-30 15:43:10 -0400

    Fized issue with Button.js

--- src/main/js/components/Button.js
+++ src/main/js/components/Button.js
@@ -1,58 +1,25 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  Animated,
-  Easing,
-  TouchableWithoutFeedback
-} from 'react-native';
+import {Button as NativeButton, StyleSheet} from 'react-native';
 
 export default class Button extends Component {
-    this.scale = new Animated.Value(0);
-
-    const buttonScale = scaleValue.interpolate({
-        inputRange: [0, 0.5, 1],
-        outputRange: [0, 1.1, 1]
-    });
 
     render() {
         return (
-            <TouchableWithoutFeedback onPress={this.props.onPress}
-            <Animated.View style={[]}>
+            <NativeButton
+                style={styles.button}
+                title={this.props.title}
+                onPress={this.props.onPress}
+            />
         );
     }
-    scale() {
-        this.scale.setValue(0);
-        Animated.timing(
-            this.scale,
-            {
-                toValue: 1,
-                duration: 300,
-                easing: Easing.easeOutBack
-            }
-        ).start();
-    }
-
-    onPress() {
-        scale();
-        props.onPress();
-    }
-
-    getContent() {
-        return <Text style=styles.title>{props.title}</Text>
-    }
-
-    const styles = StyleSheet.create({
-        title: {
-            alignItems: 'center'
-        },
-        button: {
-            alignItems: 'center',
-            justifyContext: 'center',
-            padding: 20,
-            borderWidth: 1,
-            borderColor: "FFFFFFF",
-            margin: 20
-        }
-    })
 }
+
+const styles = StyleSheet.create({
+    button: {
+        alignItems: 'center',
+        padding: 20,
+        borderWidth: 1,
+        borderColor: "#FFFFFF",
+        margin: 20
+    }
+});
