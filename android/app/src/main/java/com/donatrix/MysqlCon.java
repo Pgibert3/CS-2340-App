a57de183b89c30c60b6e59cc1ea90275a7571bec
zday123 <zday123@yahoo.com>
2018-09-30 17:55:07 -0400

    Made one method for updating data in database and one for retreiving data.

--- android/app/src/main/java/com/donatrix/MysqlCon.java
+++ android/app/src/main/java/com/donatrix/MysqlCon.java
@@ -14,7 +14,26 @@ public class MysqlCon {
 
     }
 
-    private void startConnection(String sql) {
+    private ResultSet startConnectionForData(String sql) {
+        ResultSet rs = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/Donatrix", "root",
+                    "password");
+//            System.out.println("Connected to database");
+//            System.out.println(sql);
+            Statement stmt = con.createStatement();
+//            stmt.executeUpdate(sql);
+            rs = stmt.executeQuery(sql);
+//            System.out.println(rs);
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+        return rs;
+    }
+
+    private void startConnectionForUpdate(String sql) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection(
@@ -32,12 +51,22 @@ public class MysqlCon {
     public void registerUser(String username, String password, String locked, String name) {
         String sql = String.format("insert into donatrix.users values('%s'," +
                 "'%s','%s','%s')", username, password, locked, name);
-        startConnection(sql);
+        startConnectionForUpdate(sql);
+    }
+
+    public boolean checkRegisteredUser(String username) {
+        String sql = String.format("select * from donatrix.users where email = '%s';", username);
+
+        if (startConnectionForData(sql) != null) {
+            return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
         MysqlCon me = new MysqlCon();
-        me.registerUser("dpoole@gatech.edu", "password", "0", "Davidson Poole");
+//        me.registerUser("dpoole@gatech.edu", "password", "0", "Davidson Poole");
+        me.checkRegisteredUser("dpoole@gatech.edu");
 
 //        try {
 //            Class.forName("com.mysql.jdbc.Driver");
