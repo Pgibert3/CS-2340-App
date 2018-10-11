ac285ecc880b2e463616fe1c929e2d653b884d32
Pawkwah <pjharris1998@gmail.com>
2018-10-11 13:19:21 -0400

    Fixed admin

--- android/app/src/main/java/com/donatrix/Admin.java
+++ android/app/src/main/java/com/donatrix/Admin.java
@@ -5,4 +5,33 @@ package com.donatrix;
  */
 
 public class Admin {
+    private String email;
+    private String password;
+    private String name;
+    private boolean accountState;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setAccountState(boolean accountState) {
+        this.accountState = accountState;
+    }
+    public boolean getAccountState() {
+        return this.accountState;
+    }
 }
