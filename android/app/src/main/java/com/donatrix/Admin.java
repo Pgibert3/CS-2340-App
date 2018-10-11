8cb961b216c5d12ec675c2386ac933845b54713e
zday123 <zday123@yahoo.com>
2018-10-11 14:08:13 -0400

    Damn your shitty admin commit parker

--- android/app/src/main/java/com/donatrix/Admin.java
+++ android/app/src/main/java/com/donatrix/Admin.java
@@ -5,7 +5,6 @@ package com.donatrix;
  */
 
 public class Admin {
-<<<<<<< HEAD
 
     private String name;
     private String email;
@@ -33,35 +32,5 @@ public class Admin {
 
     public void setPassword(String password) {
         this.password = password;
-=======
-    private String email;
-    private String password;
-    private String name;
-    private boolean accountState;
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getEmail() {
-        return this.email;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getPassword() {
-        return this.password;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getName() {
-        return this.name;
-    }
-    public void setAccountState(boolean accountState) {
-        this.accountState = accountState;
-    }
-    public boolean getAccountState() {
-        return this.accountState;
->>>>>>> ac285ecc880b2e463616fe1c929e2d653b884d32
     }
 }
