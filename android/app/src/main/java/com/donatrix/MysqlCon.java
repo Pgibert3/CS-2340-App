4dd6013b298db64c01fc628ad33bc94cea8c4640
zday123 <zday123@yahoo.com>
2018-10-22 15:19:50 -0400

    Worked on stuff

---
+++ android/app/src/main/java/com/donatrix/MysqlCon.java
@@ -0,0 +1,84 @@
+package com.donatrix;
+
+import java.sql.*;
+
+
+public class MysqlCon {
+
+
+    private static ResultSet startConnectionForRead(String sql) {
+        ResultSet rs = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/Donatrix", "root",
+                    "password");
+            Statement stmt = con.createStatement();
+//            stmt.executeUpdate(sql);
+            rs = stmt.executeQuery(sql);
+            System.out.println("Got here");
+//            ResultSetMetaData rsmd = rs.getMetaData();
+//            int columnNumer = rsmd.getColumnCount();
+//            while (rs.next()) {
+//                for (int i = 1; i <= columnNumer; i++ ) {
+//                    if (i > 1) System.out.print(", ");
+//                    String columnValue = rs.getString(i);
+//                    System.out.print(columnValue + " " + rsmd.getColumnName(i));
+//                }
+//                System.out.println("");
+//            }
+//            System.out.println(rs);
+        } catch(Exception e) {
+//            rs = null;
+            System.out.println(e);
+        }
+        return rs;
+    }
+
+    private static void startConnectionForWrite(String sql) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/Donatrix", "root",
+                    "password");
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate(sql);
+//            ResultSet rs = stmt.executeQuery(sql);
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+    public static void registerUser(String username, String password, String locked, String name) {
+        String sql = String.format("insert into donatrix.users values('%s'," +
+                "'%s','%s','%s')", username, password, locked, name);
+        startConnectionForWrite(sql);
+    }
+
+    public static boolean checkRegisteredUser(String username) throws SQLException {
+        String sql = String.format("select * from donatrix.users where email = '%s';", username);
+
+        if (startConnectionForRead(sql).next()) {
+            System.out.println("true");
+            return true;
+        }
+        System.out.println("False");
+        return false;
+    }
+
+    public static void main(String[] args) throws SQLException {
+//        me.registerUser("dpoole@gatech.edu", "password", "0", "Davidson Poole");
+        registerUser("pgibert@gatech.edu", "password", "0", "Paul Gibert");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/Donatrix", "root",
+                    "password");
+
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from donatrix.users where email = 'zday123@yahoo.com'");
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+}
--- android/app/src/main/java/com/donatrix/dao/UserDao.java
+++ android/app/src/main/java/com/donatrix/dao/UserDao.java
@@ -1,15 +1,17 @@
 package com.donatrix.dao;
 
 import android.content.Context;
-
+import com.donatrix.MysqlCon;
 import com.donatrix.model.UserType;
+import java.sql.*;
 
 public class UserDao {
     public static void registerUser(String email, String password, String name, boolean locked, String type, Context context) throws IllegalArgumentException {
         Database.getInstance(context).registerUser(email, password, name, locked, UserType.valueOf(type));
     }
 
-    public static boolean checkRegisteredUser(String email, String password, Context context) {
-        return Database.getInstance(context).checkRegisteredUser(email, password);
+    public static boolean checkRegisteredUser(String email, String password, Context context) throws SQLException {
+        return MysqlCon.checkRegisteredUser(email);
+//        return Database.getInstance(context).checkRegisteredUser(email, password);
     }
 }
